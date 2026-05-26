c7a7794c4f9eb13f875adfccd0872ee97491db53
Harrison O'Reibow <harrison.oreibow@gmail.com>
2026-05-26 20:18:04 +1200

    implement parser

--- python/main.py
+++ python/main.py
@@ -28,3 +28,43 @@ def is_valid_bf(char: str) -> bool:
 
 def tokenise(inp: str) -> list[TokenType]:
     return [TokenType(char) for char in inp if is_valid_bf(char)]
+
+
+def parse(toks: list[TokenType]) -> list[Command]:
+    commands: list[Command] = []
+
+    # turn tokens into commands
+    counter = 1
+    for index, tok in enumerate(toks):
+        if tok in (TokenType.JZ, TokenType.JNZ):
+            commands.append(
+                Command(tok, 0)  # this is filled in properly in the next step
+            )
+            continue
+
+        if toks[index + 1] == tok:
+            counter += 1
+        else:
+            commands.append(Command(tok, counter))
+            counter = 1
+
+    # resolve jump locations
+    loc_stack: list[int] = []
+    for index, comm in enumerate(commands):
+        match comm.tok_type:
+            case TokenType.JZ:
+                loc_stack.append(index)
+            case TokenType.JNZ:
+                loc = loc_stack.pop()
+                comm.param = loc
+                commands[loc].param = index
+
+    return commands
+
+
+if __name__ == "__main__":
+    inp = ",++[-]."
+    tokens = tokenise(inp)
+    print(tokens)
+    commands = parse(tokens)
+    print(commands)
