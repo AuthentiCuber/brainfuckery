a58a1417ce6f1edb3981a83cfd4e921b697b1eb2
Harrison O'Reibow <harrison.oreibor@gmail.com>
2026-05-29 12:59:33 +1200

    implement resolveJumps (phew)

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -1,4 +1,6 @@
 import Data.List
+import Data.Maybe
+import Data.Tuple
 
 bfChars :: [Char]
 bfChars = "><+-,.[]"
@@ -46,8 +48,27 @@ parse inp = map makeToken $ groupBy groupCond inp
     groupCond start curr = start == curr
 
 -- bachpatch jump positions
+resolveJumps' :: [(Int, Token)] -> [Int] -> [(Int, Int)] -> [(Int, Int)]
+resolveJumps' [] stack pairs = pairs
+resolveJumps' ((index, Token JZ _) : rest) stack pairs =
+  resolveJumps' rest (index : stack) pairs
+resolveJumps' ((index, Token JNZ _) : rest) stack pairs =
+  resolveJumps' rest (tail stack) ((head stack, index) : pairs)
+
 resolveJumps :: [Token] -> [Token]
-resolveJumps toks = undefined
+resolveJumps inp = map jump comms
+  where
+    comms = zip [0 ..] inp
+    jumps = filter isJump comms
+    isJump (_, Token JZ _) = True
+    isJump (_, Token JNZ _) = True
+    isJump _ = False
+    forwardIndices = resolveJumps' jumps [] []
+    revIndices = map swap forwardIndices
+    indices = forwardIndices ++ revIndices
+    jump (i, Token JZ _) = Token JZ $ fromMaybe (-1) $ lookup i indices
+    jump (i, Token JNZ _) = Token JNZ $ fromMaybe (-1) $ lookup i indices
+    jump (_, t) = t
 
 main :: IO ()
 main = putStrLn "Hello BrainFuckery!"
